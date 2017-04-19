8f98556ded2d96d2a869ee9657479ce6292a8c7f
jonpas <jonpas33@gmail.com>
2017-04-19 03:01:06 +0200

    Enable volume keys, Display 0% volume when turned off

---
+++ i3blocks/volume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+channel=$(amixer get Master)
+
+if [ $(amixer get Master | tail -c -5 | head -c -2) == "off" ]; then
+    echo "0%"
+else
+    amixer get Master | grep -E -o '[0-9]{1,3}?%' | head -1
+fi
+
