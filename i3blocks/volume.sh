7270db397c16eb59ea64b46a236b7680e9578ce3
jonpas <jonpas33@gmail.com>
2017-04-19 13:17:29 +0200

    Fix battery charging block, Show volume muted

--- i3blocks/volume.sh
+++ i3blocks/volume.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+volume=$(amixer get Master | grep -E -o '[0-9]{1,3}?%' | head -c -2)
+
 if [ $(amixer get Master | tail -c -5 | head -c -2) == "off" ]; then
-    echo "0%"
+    echo " $volume"
+    exit 0
+fi
+
+if [ $volume -ge 50 ]; then
+    echo " $volume"
 else
-    amixer get Master | grep -E -o '[0-9]{1,3}?%' | head -1
+    echo " $volume"
 fi
