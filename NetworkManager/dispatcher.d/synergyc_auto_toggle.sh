3a32fbdf513c305b07c3915cf2d8471211a6a094
jonpas <jonpas33@gmail.com>
2017-04-19 14:57:12 +0200

    Add synergyc auto toggle on home ethernet

---
+++ NetworkManager/dispatcher.d/synergyc_auto_toggle.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Must be owned by root!
+
+if [ "$CONNECTION_UUID" = "71f4b399-576b-3081-b528-a0eb5b7992f0" ]; then
+    case "$2" in
+        up)
+            # Wait for nm-applet to start (during boot)
+            while [ -z $(pidof nm-applet) ]; do
+                sleep 3;
+            done
+
+            su jonpas -c "synergyc -d WARNING 192.168.1.30"
+            ;;
+        down)
+            pkill -x synergyc
+            ;;
+    esac
+fi
