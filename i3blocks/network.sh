eb16e959e6118586695218357cd8804ee6089753
jonpas <jonpas33@gmail.com>
2017-04-24 20:15:16 +0200

    Rework network i3block to support any IP address, Prioritize Ethernet over Wi-Fi properly

--- i3blocks/network.sh
+++ i3blocks/network.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-address=$(ip addr | grep -o '192\..*\..*\..*/' | head -c -2)
+# Ethernet
+address=$(ip addr show enp0s25 | grep -Po 'inet \K[\d.]+')
+if [ -n "$address" ]; then
+    echo " $address"
+    echo " $address"
+    echo "#b8bb26"
+    exit 0
+fi
 
-if [ $(iwconfig | grep -o ESSID.*) != "ESSID:off/any" ]; then
-    # Wi-Fi
+# Wi-Fi
+address=$(ip addr show wlp3s0 | grep -Po 'inet \K[\d.]+')
+if [ -n "$address" ]; then
     ssid=$(iwconfig | grep -o ESSID.* | tail -c +8 | head -c -4)
-
     echo " $address ($ssid)"
     echo " $ssid"
     echo "#b8bb26"
-elif [ $(cat /sys/class/net/enp0s25/operstate) == "up" ]; then
-    # Ethernet
-    echo " $address"
-    echo " $address"
-    echo "#b8bb26"
-else
-    # Down
-    echo " down"
-    echo " down"
-    echo "#fb4934"
+    echo 0
 fi
+
+# Down
+echo " down"
+echo " down"
+echo "#fb4934"
