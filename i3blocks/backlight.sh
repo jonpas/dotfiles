ede11ff7369974917030a0f96e8cbe66a5594fe8
jonpas <jonpas33@gmail.com>
2017-06-07 16:22:07 +0200

    Wait for backlight hardware to actually apply value before showing in i3bar

--- i3blocks/backlight.sh
+++ i3blocks/backlight.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 0.1 # Wait for hardware to actually apply values
+
 actual_brightness=$(cat /sys/class/backlight/acpi_video0/actual_brightness)
 max_brightness=$(cat /sys/class/backlight/acpi_video0/max_brightness)
 
