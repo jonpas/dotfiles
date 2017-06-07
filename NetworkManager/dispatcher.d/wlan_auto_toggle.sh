1d29f0ba955f4d5271bd2b23bdad6b010334420b
jonpas <jonpas33@gmail.com>
2017-06-07 17:59:41 +0200

    Revert Wi-Fi auto-toggle on ethernet plug

--- NetworkManager/dispatcher.d/wlan_auto_toggle.sh
+++ NetworkManager/dispatcher.d/wlan_auto_toggle.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Must be owned by root!
 
-if [[ "$1" == "enp0s25" ]] && [[ "$2" == "up" ]] ; then
-    nmcli radio wifi off
-elif [ $(nmcli radio wifi) == "disabled" ]; then
-    nmcli radio wifi on
+if [ "$1" = "enp0s25" ]; then
+    case "$2" in
+        up)
+            nmcli radio wifi off
+            ;;
+        down)
+            nmcli radio wifi on
+            ;;
+    esac
 fi
