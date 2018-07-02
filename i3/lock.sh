7a1c96825beb4e29f44347c6994ef8feb22f909e
jonpas <jonpas33@gmail.com>
2018-07-02 20:49:06 +0200

    Bind G710 'G1' key to open mount/media location, Fix lock script for new monitors

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 scrot -q 1 /tmp/screenshot.jpg
-if [[ $(hostname) == "ancient" ]]
-then
+if [ $(hostname) = "ancient" ]; then
     convert /tmp/screenshot.jpg -resize 77x45 -scale 1440x900 /tmp/screenshotblur.png
 else
-    convert /tmp/screenshot.jpg -resize 77x45 -scale 3200x1080 /tmp/screenshotblur.png
+    convert /tmp/screenshot.jpg -resize 200x130 -scale 3600x1080 /tmp/screenshotblur.png
 fi
 i3lock -i /tmp/screenshotblur.png
