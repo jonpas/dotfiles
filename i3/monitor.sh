001dd6cf6311262f0900c2926f91ddc740bf410c
jonpas <jonpas33@gmail.com>
2018-07-10 21:42:05 +0200

    Kill synergy server process before starting it in monitor script

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -2,5 +2,8 @@
 
 if [ $(hostname) = "loki" ]; then
     xrandr --output HDMI2 --primary --output VGA1 --right-of HDMI2
-    synergys -d WARNING # After setting monitors, so Synergy doesn't get confused, no SSL due to connection issues
+
+    # After setting monitors, so Synergy doesn't get confused, no SSL due to connection issues
+    pkill -x synergys
+    synergys -d WARNING
 fi
