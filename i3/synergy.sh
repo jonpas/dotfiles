5453cc71a8f5db1b3537a3e764daaf99e3043284
jonpas <jonpas33@gmail.com>
2018-07-21 01:03:24 +0200

    Setup Synergy auto-reconfiguration on monitor changes

--- i3/synergy.sh
+++ i3/synergy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 if [ $(hostname) = "loki" ]; then
+    vm=$1
+
+    # Change Synergy configuration for VM in the center
+    if [ "$vm" = "center" ]; then
+        config_vm_center=$(cat ~/.config/i3/synergy-vm-center.conf)
+        perl -i.bak -0777pe "s/section: links\n(.+?)\nend/$config_vm_center/s" ~/dotfiles/synergy.conf
+    fi
+
     pkill -x synergys
     synergys -d WARNING # No SSL due to connection issues
+
+    # Restore Synergy configuration
+    if [ "$vm" = "center" ]; then
+        rm ~/dotfiles/synergy.conf
+        mv ~/dotfiles/synergy.conf.bak ~/dotfiles/synergy.conf
+    fi
 fi
