d8792202cf7709ab38439ee69d927823f59b415d
jonpas <jonpas33@gmail.com>
2018-07-21 19:29:22 +0200

    Auto-start applications on i3 workspace 1 on loki

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -5,4 +5,10 @@ if [ $(hostname) = "loki" ]; then
 
     # Synergy gets confused with multiple monitors, so restart it
     . ~/.config/i3/synergy.sh
+
+    # Auto-start programs on workspace 1
+    i3-msg "workspace 1  ; append_layout ~/.config/i3/workspace-1.json"
+    termite -e htop &
+    slack &
+    discord &
 fi
