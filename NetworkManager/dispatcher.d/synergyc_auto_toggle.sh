0ba8748940dbef16d041137b073a6927f0350429
jonpas <jonpas33@gmail.com>
2018-11-30 17:29:19 +0100

    Fix Synergy auto toggle on odin

--- NetworkManager/dispatcher.d/synergyc_auto_toggle.sh
+++ NetworkManager/dispatcher.d/synergyc_auto_toggle.sh
@@ -2,13 +2,13 @@
 # Must be owned by root!
 
 con_uuid_ancient="71f4b399-576b-3081-b528-a0eb5b7992f0"
-con_uuid_odin="21c07d29-4d48-42c2-819e-c71dd162028f"
+con_uuid_odin="803105d7-5b6f-3b65-bdb6-5566b2d07a96"
 
 if [ $(hostname) = "loki" ]; then
     exit 0
 fi
 
-if [ "$CONNECTION_UUID" != $con_uuid_ancient ] && [ "$CONNECTION_UUID" != $con_uuid_odin ]; then
+if [ "$CONNECTION_UUID" != "$con_uuid_ancient" ] && [ "$CONNECTION_UUID" != "$con_uuid_odin" ]; then
     exit 0
 fi
 
@@ -19,7 +19,7 @@ case "$2" in
             sleep 3;
         done
 
-        su jonpas -c "synergyc -d WARNING loki"
+        su jonpas -c "synergyc -d WARNING 192.168.178.30" # Synergy does not like IPv6
         ;;
     down)
         pkill -x synergyc
