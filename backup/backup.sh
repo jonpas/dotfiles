e571eb71a62e7594474d8af62378df2d87892828
jonpas <jonpas33@gmail.com>
2018-11-30 01:12:37 +0100

    Fix backup script file naming

--- backup/backup.sh
+++ backup/backup.sh
@@ -10,7 +10,7 @@ exclude_file="./backup-exc.txt"
 # Labels for backup name
 distro=$(cat /etc/*-release | grep "ID=" | awk -F"=" '{print $2}')
 date=$(date +%Y%m%dT%H%M%S)
-backupfile="$backdest/fsbackup_$(hostname)_${distro}_${date}.tar.gz"
+backupfile="$backdest/fsbackup_$(hostname)_$distro_$date.tar.gz"
 
 # Check if running as root
 if [ "$EUID" -ne 0 ]; then
