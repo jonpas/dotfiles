4125d1c8f8027df5c6a5c5cc4b36a00c7055b4ee
jonpas <jonpas33@gmail.com>
2019-03-05 10:23:28 +0100

    Optimize bandwidth i3block script

--- i3blocks/bandwidth.sh
+++ i3blocks/bandwidth.sh
@@ -2,6 +2,7 @@
 # Copyright (C) 2012 Stefan Breunig <stefan+measure-net-speed@mathphys.fsk.uni-heidelberg.de>
 # Copyright (C) 2014 kaueraal
 # Copyright (C) 2015 Thiago Perrotta <perrotta dot thiago at poli dot ufrj dot br>
+# Copyright (C) 2019 Jonpas
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,92 +17,69 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Get custom IN and OUT labels if provided by command line arguments
-while [[ $# -gt 1 ]]; do
-    key="$1"
-    case "$key" in 
-        -i|--inlabel)
-            INLABEL="$2"
-            shift;;
-        -o|--outlabel)
-            OUTLABEL="$2"
-            shift;;
-    esac
-    shift
-done
+# Use the device used for the default route
+interface=$(ip route | awk '/^default/ {print $5}')
 
-[[ -z $INLABEL ]] && INLABEL="IN "
-[[ -z $OUTLABEL ]] && OUTLABEL="OUT "
-
-# Use the provided interface, otherwise the device used for the default route.
-if [[ -n $BLOCK_INSTANCE ]]; then
-  INTERFACE=$BLOCK_INSTANCE
-else
-  INTERFACE=$(ip route | awk '/^default/ { print $5 ; exit }')
-fi
-
-# Issue #36 compliant.
-if ! [ -e "/sys/class/net/${INTERFACE}/operstate" ] || ! [ "`cat /sys/class/net/${INTERFACE}/operstate`" = "up" ]
-then
-    echo "$INTERFACE down"
-    echo "$INTERFACE down"
-    echo "#FF0000"
+# No connection
+if ! [ -e "/sys/class/net/${interface}/operstate" ] || ! [ "`cat /sys/class/net/${interface}/operstate`" = "up" ]; then
+    echo "     "
+    echo "     "
+    echo "#fb4934"
     exit 0
 fi
 
-# path to store the old results in
-path="/dev/shm/$(basename $0)-${INTERFACE}"
+# Path to store the old results in
+path="/dev/shm/$(basename $0)-${interface}"
 
-# grabbing data for each adapter.
-read rx < "/sys/class/net/${INTERFACE}/statistics/rx_bytes"
-read tx < "/sys/class/net/${INTERFACE}/statistics/tx_bytes"
+# Grab data for each adapter
+read rx < "/sys/class/net/${interface}/statistics/rx_bytes"
+read tx < "/sys/class/net/${interface}/statistics/tx_bytes"
 
-# get time
+# Get time
 time=$(date +%s)
 
-# write current data if file does not exist. Do not exit, this will cause
-# problems if this file is sourced instead of executed as another process.
+# Write current data if file does not exist
+# Do not exit, would cause problems if this file is
+# sourced instead of executed as another process
 if ! [[ -f "${path}" ]]; then
-  echo "${time} ${rx} ${tx}" > "${path}"
-  chmod 0666 "${path}"
+    echo "${time} ${rx} ${tx}" > "${path}"
+    chmod 0666 "${path}"
 fi
 
-# read previous state and update data storage
+# Read previous state and update data storage
 read old < "${path}"
 echo "${time} ${rx} ${tx}" > "${path}"
 
-# parse old data and calc time passed
+# Parse old data and calculate time passed
 old=(${old//;/ })
 time_diff=$(( $time - ${old[0]} ))
 
-# sanity check: has a positive amount of time passed
+# Sanity check - positive amount of time passed
 [[ "${time_diff}" -gt 0 ]] || exit
 
-# calc bytes transferred, and their rate in byte/s
+# Calculate bytes transferred, and their rate in byte/s
 rx_diff=$(( $rx - ${old[1]} ))
 tx_diff=$(( $tx - ${old[2]} ))
 rx_rate=$(( $rx_diff / $time_diff ))
 tx_rate=$(( $tx_diff / $time_diff ))
 
-# shift by 10 bytes to get KiB/s. If the value is larger than
-# 1024^2 = 1048576, then display MiB/s instead
+# Shift by 10 bytes to get KiB/s
+# If the value is larger than 1024^2 = 1048576, display MiB/s instead
 
-# incoming
-echo -n "$INLABEL"
+# Incoming
+echo -n " "
 rx_kib=$(( $rx_rate >> 10 ))
 if hash bc 2>/dev/null && [[ "$rx_rate" -gt 1048576 ]]; then
-  printf '%sM' "`echo "scale=1; $rx_kib / 1024" | bc`"
+    printf '%sM' "`echo "scale=1; $rx_kib / 1024" | bc`"
 else
-  echo -n "${rx_kib}K"
+    echo -n "${rx_kib}K"
 fi
 
-echo -n " "
-
-# outgoing
-echo -n "$OUTLABEL"
+# Outgoing
+echo -n "  "
 tx_kib=$(( $tx_rate >> 10 ))
 if hash bc 2>/dev/null && [[ "$tx_rate" -gt 1048576 ]]; then
-  printf '%sM' "`echo "scale=1; $tx_kib / 1024" | bc`"
+    printf '%sM' "`echo "scale=1; $tx_kib / 1024" | bc`"
 else
-  echo -n "${tx_kib}K"
+    echo -n "${tx_kib}K"
 fi
