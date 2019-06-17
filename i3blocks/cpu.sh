4eea5d5cc2ecd674b8d26e6b8d305d3cf7dab659
jonpas <jonpas33@gmail.com>
2019-06-17 14:24:08 +0200

    Add sound notification to too high CPU usage, CPU temp and Memory usage, Improve Memory usage accuracy

--- i3blocks/cpu.sh
+++ i3blocks/cpu.sh
@@ -4,6 +4,10 @@ usage=$(mpstat 1 | head -4 | tail -1 | awk '{print int(100-$12+0.5)}')
 
 echo " $usage%"
 
+if [ $usage -ge 95 ]; then
+    play --no-show-progress --null --channels 1 synth 0.4 sine 900 # play from SoX
+fi
+
 if [ $usage -ge 80 ]; then
     exit 33
 fi
--- i3blocks/cpu_temp.sh
+++ i3blocks/cpu_temp.sh
@@ -4,6 +4,10 @@ temp=$(sensors | grep -o +[0-9]* | head -1 | tail -c +2)
 
 echo " $temp°C"
 
+if [ $usage -ge 90 ]; then
+    play --no-show-progress --null --channels 1 synth 0.4 sine 900 # play from SoX
+fi
+
 if [ $temp -ge 70 ]; then
     exit 33
 fi
