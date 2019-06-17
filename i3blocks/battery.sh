f5e883bfd4b926624294f108865e2cdc41706892
jonpas <jonpas33@gmail.com>
2019-06-17 13:30:10 +0200

    Show reamining battery time in i3

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -12,25 +12,27 @@ if [[ $stat == "Charging," ]] || [[ $stat == "Full," ]]; then
     exit 0
 fi
 
+remaining=$(acpi -b | awk '{print $5}' | head -c 5)
+
 if [ $charge -ge 80 ]; then
-    echo " $charge%"
+    echo " $charge% ($remaining)"
     exit 0
 fi
 
 if [ $charge -ge 60 ]; then
-    echo " $charge%"
+    echo " $charge% ($remaining)"
     exit 0
 fi
 
 if [ $charge -ge 40 ]; then
-    echo " $charge%"
+    echo " $charge% ($remaining)"
     exit 0
 fi
 
 if [ $charge -ge 20 ]; then
-    echo " $charge%"
+    echo " $charge% ($remaining)"
     exit 0
 fi
 
-echo " $charge%"
+echo " $charge% ($remaining)"
 exit 33
