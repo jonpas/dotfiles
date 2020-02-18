873f8fa36e35817c17591465b8794d320edd32c5
jonpas <jonpas33@gmail.com>
2020-02-18 05:13:49 +0100

    Remove memory sound warning (i3block)

--- i3blocks/memory.sh
+++ i3blocks/memory.sh
@@ -4,10 +4,6 @@ usage=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
 
 echo " $usage%"
 
-if [ $usage -ge 95 ]; then
-    play --no-show-progress --null --channels 1 synth 0.4 sine 900 # play from SoX
-fi
-
 if [ $usage -ge 90 ]; then
     exit 33
 fi
