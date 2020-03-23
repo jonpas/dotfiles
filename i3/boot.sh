a2a3efd0f35899b1b22cb0144aa7dca4468f0fa7
jonpas <jonpas33@gmail.com>
2020-03-23 16:52:59 +0100

    Rework program auto-starting Use workspace 10 for monitoring (htop and watching) with monitoring terminal. Use workspace 1 for chat only (Slack, Discord and MS Teams). Start Firefox on workspace 2.

--- i3/boot.sh
+++ i3/boot.sh
@@ -11,12 +11,17 @@ if [ $(hostname) = "loki" ]; then
     sleep 3 && . ~/.config/i3/monitor.sh &
 fi
 
-# Auto-start programs on workspace 1
-# Sleep between terminal programs to assure correct slot is taken (same terminal)
-i3-msg "workspace 1  ; append_layout ~/.config/i3/workspace-1.json"
+# Auto-start programs
+i3-msg "workspace 10  ; append_layout ~/.config/i3/workspace-10.json"
+i3-sensible-terminal -t "Terminal: monitor" &
+i3-sensible-terminal -e htop -t "Terminal: htop" &
+i3-sensible-terminal -e 'watch progress -q' -t "Terminal: progress" &
+i3-sensible-terminal -e 'watch grep \"cpu MHz\" /proc/cpuinfo' -t "Terminal: cpu freq" &
 
-i3-sensible-terminal -e htop & sleep 0.25
-i3-sensible-terminal -e 'watch progress -q' & sleep 0.25
-i3-sensible-terminal -e 'watch grep \"cpu MHz\" /proc/cpuinfo' &
+i3-msg "workspace 1  ; append_layout ~/.config/i3/workspace-1.json"
 slack &
 discord &
+teams &
+
+i3-msg "workspace 2  ; append_layout ~/.config/i3/workspace-2.json"
+firefox &
