4a4a525951253966cf1e7df16fa8cf78a8ed6fd8
jonpas <jonpas33@gmail.com>
2020-03-25 16:35:02 +0100

    Start Chromium in tabbed layout with MS Teams

--- i3/boot.sh
+++ i3/boot.sh
@@ -22,6 +22,7 @@ i3-msg "workspace 1  ; append_layout ~/.config/i3/workspace-1.json"
 slack &
 discord &
 teams &
+chromium &
 
 i3-msg "workspace 2  ; append_layout ~/.config/i3/workspace-2.json"
 firefox &
