7d536231ffca397e365b1afb161cca9782c94515
jonpas <jonpas33@gmail.com>
2020-04-01 10:40:02 +0200

    Convert WinVM second disk from qcow2 to raw image

--- vm/win10-pci.sh
+++ vm/win10-pci.sh
@@ -138,8 +138,9 @@ OPTS+=" -drive if=pflash,format=raw,file=/home/jonpas/images/vm/OVMF_VARS-win10-
 # Drives
 # Create image: qemu-img create -f raw name.img 10G -o preallocation=full (-o cluster_size=16K for qcow2)
 # Convert qcow2 to raw: qemu-img convert -p -O raw source.qcow2 target.img -o preallocation=full
+# Resize image: qemu-img resize -f raw --preallocation=full source.img +5G
 OPTS+=" -drive file=/home/jonpas/images/vm/win10-ovmf.img,format=raw,index=0,media=disk,if=virtio,aio=native,cache=none"
-OPTS+=" -drive file=/home/jonpas/Data/images/vm/data.qcow2,format=qcow2,index=1,media=disk,if=virtio,aio=native,cache=none"
+OPTS+=" -drive file=/home/jonpas/Data/images/vm/data.img,format=raw,index=1,media=disk,if=virtio,aio=native,cache=none"
 OPTS+=" -drive file=/home/jonpas/Data/images/windows10.iso,index=2,media=cdrom"
 OPTS+=" -drive file=/home/jonpas/Data/images/virtio-win.iso,index=3,media=cdrom"
 
