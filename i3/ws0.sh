9ffad70cecef8b1d7a2753c95f6fee0dcb3ce5d8
jonpas <jonpas33@gmail.com>
2020-05-10 01:26:18 +0200

    Rework default workspace setup

--- i3/ws0.sh
+++ i3/ws0.sh
@@ -1,8 +1,7 @@
 i3-msg "workspace 0  ; append_layout ~/.config/i3/workspace-0.json"
 
-i3-sensible-terminal -t "Terminal: monitor" &
 i3-sensible-terminal -e htop -t "Terminal: htop" &
+i3-sensible-terminal -e 'watch -t "grep \"cpu MHz\" /proc/cpuinfo && sensors -A coretemp-isa-0000"' -t "Terminal: sensors" &
 i3-sensible-terminal -e 'watch -t progress -q' -t "Terminal: progress" &
-i3-sensible-terminal -e 'watch -t grep \"cpu MHz\" /proc/cpuinfo' -t "Terminal: cpu freq" &
-i3-sensible-terminal -e 'watch -t sensors -A coretemp-isa-0000' -t "Terminal: sensors" &
 i3-sensible-terminal -t 'Terminal: WinVM' &
+firefox --new-window about:logo &
--- i3/ws1.sh
+++ i3/ws1.sh
@@ -1,5 +1,5 @@
 i3-msg "workspace 1  ; append_layout ~/.config/i3/workspace-1.json"
 
+slack &
 discord &
-firefox --new-window about:logo & # Chat
-firefox --new-window about:mozilla & # Ext
+chromium &
