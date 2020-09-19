7e4639243c6a85a3c78f08e2b50e292b1544e327
jonpas <jonpas33@gmail.com>
2020-09-19 06:02:46 +0200

    Improve lock handling further

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+dark=$1
+
+# Create lock screen
 target=$(xrandr | tr "," "\n" | sed -n '2p' | tail -c +10)
 targetX=$(awk '{print $1}' <<< $target)
 targetY=$(awk '{print $3}' <<< $target)
@@ -11,13 +14,14 @@ scrot -o -q 1 /tmp/screenshot.jpg
 convert /tmp/screenshot.jpg -resize ${resizeX}x${resizeY} -scale ${targetX}x${targetY} /tmp/screenshotblur.png
 
 # Lock
-if [ $(hostname) = "loki" ]; then
-    . ~/.config/i3/rgb.sh off
+if [ "$dark" = "true" ]; then
+    xset dpms force suspend &&
+    . ~/.config/i3/rgb.sh off &
 fi
 
 i3lock -i /tmp/screenshotblur.png --nofork # no fork waits for unlock to continue executing
 
 # Unlock
-if [ $(hostname) = "loki" ]; then
+if [ "$dark" = "true" ]; then
     . ~/.config/i3/rgb.sh on
 fi
