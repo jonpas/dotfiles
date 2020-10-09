a324958afda1cdf38904d7e0cab33a2e40d6e125
jonpas <jonpas33@gmail.com>
2020-10-09 09:40:04 +0200

    Optimize workspace-0 and fix sensors on odin

--- i3/ws0.sh
+++ i3/ws0.sh
@@ -1,7 +1,13 @@
 i3-msg "workspace 0  ; append_layout ~/.config/i3/workspace-0.json"
 
 i3-sensible-terminal -e htop -t "Terminal: htop" &
-i3-sensible-terminal -e 'watch -t "sensors zenpower-pci-00c3 -A | grep T"' -t "Terminal: sensors" &
+
+if [ $(hostname) = "loki" ]; then
+    i3-sensible-terminal -e 'watch -t "sensors zenpower-pci-00c3 -A | grep °C"' -t "Terminal: sensors" &
+else
+    i3-sensible-terminal -e 'watch -t "sensors coretemp-isa-0000 -A | grep °C"' -t "Terminal: sensors" &
+fi
+
 i3-sensible-terminal -e 'watch -t progress -q' -t "Terminal: progress" &
 i3-sensible-terminal -t 'Terminal: WinVM' &
 firefox --new-window about:logo &
