dfa9aafdbe5597ab365e9c658b6e59643c09c821
jonpas <jonpas33@gmail.com>
2020-11-03 02:49:20 +0100

    Fix RGB for OpenRGB 0.4

--- i3/rgb.sh
+++ i3/rgb.sh
@@ -4,8 +4,8 @@ if [ $(hostname) = "loki" ]; then
     state=$1
 
     if [ "$state" = "on" ]; then
-        OpenRGB -p ~/dotfiles/OpenRGB/Wraith.orp
+        openrgb -p ~/dotfiles/OpenRGB/Wraith.orp
     elif [ "$state" = "off" ]; then
-        OpenRGB -p ~/dotfiles/OpenRGB/Off.orp
+        openrgb -p ~/dotfiles/OpenRGB/Off.orp
     fi
 fi
