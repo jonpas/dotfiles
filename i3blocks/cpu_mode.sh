1dbb78cf34852c1180644ccfeac83ff8c7ccbaf4
jonpas <jonpas33@gmail.com>
2020-11-16 18:13:16 +0100

    Add CPU mode (sets governor or opens zenmonitor) i3block

---
+++ i3blocks/cpu_mode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $(hostname) != "loki" ]; then
+    exit 0
+fi
+
+governor=$(cpupower frequency-info -p | grep -o '".*"' | tr -d '"')
+
+if [ ! -z "${BLOCK_BUTTON}" ]; then
+    if [ "${BLOCK_BUTTON}" == "3" ]; then
+        zenmonitor
+    elif [ "${BLOCK_BUTTON}" == "2" ]; then
+        notify-send "CPU governor is \"$governor\""
+    else
+        if [ "$governor" == "schedutil" ]; then
+            governor="performance"
+        elif [ "$governor" == "performance" ]; then
+            governor="schedutil"
+        fi
+        i3-sensible-terminal -e "sudo cpupower frequency-set -g $governor"
+        notify-send "CPU governor set to \"$governor\""
+    fi
+fi
+
+echo ""
+echo ""
+
+if [ "$governor" == "performance" ]; then
+    echo "#b8bb26"
+else
+    echo "#a9a9a9"
+fi
