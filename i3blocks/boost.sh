7df4e621ac29832a89e12e1673b8477e9bf3c5ae
jonpas <jonpas33@gmail.com>
2020-11-17 01:22:56 +0100

    Simplify Boost i3block

--- i3blocks/boost.sh
+++ i3blocks/boost.sh
@@ -7,23 +7,20 @@ fi
 governor=$(cpupower frequency-info -p | grep -o '".*"' | tr -d '"')
 
 if [ ! -z "${BLOCK_BUTTON}" ]; then
-    if [ "${BLOCK_BUTTON}" == "3" ]; then
-        zenmonitor
-    elif [ "${BLOCK_BUTTON}" == "2" ]; then
-        notify-send "CPU governor is \"$governor\""
-    else
-        if [ "$governor" == "schedutil" ]; then
-            governor="performance"
-        elif [ "$governor" == "performance" ]; then
-            governor="schedutil"
-        fi
-        i3-sensible-terminal -e "sudo cpupower frequency-set -g $governor"
+    governor_set="schedutil"
+    if [ "$governor" == "schedutil" ]; then
+        governor_set="performance"
+    fi
+
+    i3-sensible-terminal -e "sudo cpupower frequency-set -g $governor_set"
+
+    if [ "$(cpupower frequency-info -p | grep -o '".*"' | tr -d '"')" != "$governor" ]; then
+        governor=$governor_set
     fi
 fi
 
 echo ""
 echo ""
-
 if [ "$governor" == "performance" ]; then
     echo "#b8bb26"
 else
