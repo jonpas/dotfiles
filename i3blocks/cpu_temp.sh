031fc48d999abd2846b96e2ffca76a5a62f90ec5
jonpas <jonpas33@gmail.com>
2021-03-25 13:15:48 +0100

    Fix CPU temperature i3block on loki

--- i3blocks/cpu_temp.sh
+++ i3blocks/cpu_temp.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-temp=$(sensors | grep -o +[0-9]* | head -1 | tail -c +2)
+if [ $(hostname) = "loki" ]; then
+    temp=$(sensors | grep -o "+[0-9]*" | head -3 | tail -1 | tail -c +2)
+else
+    temp=$(sensors | grep -o "+[0-9]*" | head -1 | tail -c +2)
+fi
 
 echo " $temp°C"
 
-if [ $usage -ge 90 ]; then
+if [ $temp -ge 90 ]; then
     play --no-show-progress --null --channels 1 synth 0.4 sine 900 # play from SoX
 fi
 
