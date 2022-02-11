14562af7c686081894541204bc400df38b0a2322
jonpas <jonpas33@gmail.com>
2022-02-11 19:11:02 +0100

    Notify on critical memory usage

--- i3blocks/memory.sh
+++ i3blocks/memory.sh
@@ -5,5 +5,9 @@ usage=$(free | grep Mem | awk '{print int($3/$2 * 100)}')
 echo " $usage%"
 
 if [ $usage -ge 90 ]; then
+    notify-send -u critical "MEM CRITICAL" "Memory at $usage%"
+fi
+
+if [ $usage -ge 85 ]; then
     exit 33
 fi
