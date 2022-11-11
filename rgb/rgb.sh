1eb7dac89e5e933329b1edd9f46373ac62c658ca
jonpas <jonpas33@gmail.com>
2022-11-11 03:10:21 +0100

    Add G710+ lighting to RGB script

--- rgb/rgb.sh
+++ rgb/rgb.sh
@@ -3,9 +3,22 @@
 if [ $(hostname) = "loki" ]; then
     state=$1
 
+    # G710+ find LED class
+    g710_vpid="046D:C24D"
+    g710_keys=$(find /sys/class/leds/ -lname "*$g710_vpid*:white:keys")
+    g710_wasd=$(find /sys/class/leds/ -lname "*$g710_vpid*:white:wasd")
+
     if [ "$state" = "on" ]; then
         openrgb -p loki.orp
+
+        # G710+ Driver Kernel API
+        echo 2 > $g710_keys/brightness
+        echo 3 > $g710_wasd/brightness
     elif [ "$state" = "off" ]; then
         openrgb -p off.orp
+
+        # G710+ Driver Kernel API
+        echo 0 > $g710_keys/brightness
+        echo 0 > $g710_wasd/brightness
     fi
 fi
