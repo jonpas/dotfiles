76f1916af5ec19d5ef64efed4e484d2a51eae46a
jonpas <jonpas33@gmail.com>
2022-11-14 20:41:09 +0100

    Update monitors setup and reorganize logic

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -4,38 +4,65 @@ if [ $(hostname) = "loki" ]; then
     layout=$1
 
     global="--dpi 96"
-    LGWcfg="--mode 2560x1080 --rate 75"
-    LGWp="HDMI-A-0"
-    IIYcfg="--mode 1920x1080 --rate 60"
-    IIYp="DisplayPort-0"
-    SSMcfg="--mode 1680x1050 --rate 60 --rotate left"
-    SSMp="DVI-D-0"
+    p1="DisplayPort-0"
+    c1="--mode 3440x1440 --rate 144" # 34" UW 1440p 144 Hz
+    s1="1174021700438" # iiyama GB3466WQSU-B1
+    s1hdmi1="0x11"
+    s1hdmi2="0x12"
+    s1dp1="0x0f"
+    s1dp2="0x10"
+
+    p2="HDMI-A-0"
+    c2="--mode 2560x1080 --rate 60" # 29" UW 1080p 75 Hz
+    s2="" # DDC doesn't work - LG 29UM69G (ddcutil detect cannot even read serial)
+
+    p3="DVI-D-0"
+    c3="--mode 1920x1080 --rate 60" # 24" 1080p 60 Hz
+    s3="1130751321527" # iiyama ProLite B2483HS
+    s3hdmi="0x11"
+    s3dvi="0x03"
+    s3vga="0x01"
 
     if [ -z "$layout" ]; then
-        ddcutil --sn 1130751321527 setvcp 60 0x03 # DVI
+        ddcutil --sn $s1 setvcp 60 $s1dp1
+        #ddcutil --sn $s2 setvcp 60 $s2hdmi
+        ddcutil --sn $s3 setvcp 60 $s3dvi
         xrandr $global \
-            --output $LGWp $LGWcfg --pos 0x1080 --primary \
-            --output $IIYp $IIYcfg --pos 500x0 \
-            --output $SSMp $SSMcfg --pos 2560x480 --rotate left
+            --output $p1 $c1 --pos 0x$(( 1080 - 280 )) --primary \
+            --output $p2 $c2 --pos 3440x1080 \
+            --output $p3 $c3 --pos 3440x0
     elif [ "$layout" = "right" ]; then
-        ddcutil --sn 1130751321527 setvcp 60 0x11 # HDMI
+        ddcutil --sn $s1 setvcp 60 $s1dp2
+        #ddcutil --sn $s2 setvcp 60 $s2hdmi
+        ddcutil --sn $s3 setvcp 60 $s3dvi
         xrandr $global \
-            --output $LGWp --off \
-            --output $IIYp --off \
-            --output $SSMp --primary
+            --output $p1 --off \
+            --output $p2 $c2 --pos 0x1080 --primary \
+            --output $p3 $c3 --pos 0x0
+    elif [ "$layout" = "left" ]; then
+        ddcutil --sn $s1 setvcp 60 $s1dp1
+        #ddcutil --sn $s2 setvcp 60 $s2dp
+        ddcutil --sn $s3 setvcp 60 $s3dvi
+        xrandr $global \
+            --output $p1 $c1 --pos 0x$(( 1080 - 280 )) --primary \
+            --output $p2 --off \
+            --output $p3 $c3 --pos 3440x0
     elif [ "$layout" = "up" ]; then
-        ddcutil --sn 1130751321527 setvcp 60 0x03 # DVI
+        ddcutil --sn $s1 setvcp 60 $s1dp2
+        #ddcutil --sn $s2 setvcp 60 $s2dp
+        ddcutil --sn $s3 setvcp 60 $s3dvi
         xrandr $global \
-            --output $LGWp --off \
-            --output $SSMp $SSMcfg --pos 0x0 \
-            --output $IIYp $IIYcfg --pos 1050x600 --primary
+            --output $p1 --off \
+            --output $p2 --off \
+            --output $p3 $c3 --pos 0x0 --primary
     elif [ "$layout" = "down" ]; then
-        #ddcutil --sn 1130751321527 setvcp 60 0x11 # HDMI
-        ddcutil --sn 1130751321527 setvcp 60 0x01 # VGA
+        ddcutil --sn $s1 setvcp 60 $s1dp1
+        #ddcutil --sn $s2 setvcp 60 $s2hdmi
+        ddcutil --sn $s3 setvcp 60 $s3hdmi
         xrandr $global \
-            --output $LGWp $LGWcfg --pos 0x600 --primary \
-            --output $IIYp --off \
-            --output $SSMp $SSMcfg --pos 2560x0
+            --output $p1 $c1 --pos 0x0 --primary \
+            --output $p2 $c2 --pos 3440x285 \
+            --output $p3 --off
     fi
 fi
 
