00784b11092127924e4f92032209d4feb13eb5f7
jonpas <jonpas33@gmail.com>
2022-11-14 16:02:52 +0100

    Add SMBIOS/DMI data from host and convert opts handling to array in WinVM

--- vm/win-pci.sh
+++ vm/win-pci.sh
@@ -17,6 +17,12 @@ MEMORY="32"
 PCI_GPU_VIDEO=0000:0a:00.0
 PCI_GPU_AUDIO=0000:0a:00.1
 
+smbios() {
+    dmi_string="$1"
+    dmi_value=$(dmidecode -s "$dmi_string")
+    echo ${dmi_value//,/,,} # Escape comma
+}
+
 rebind() {
     dev="$1"
     driver="$2"
@@ -112,22 +118,27 @@ fi
 
 
 # VM INIT
-OPTS=""
+OPTS=()
 
-#OPTS+=" -runas jonpas"
+#OPTS+=(-runas $(whoami))
 
 # General
-OPTS+=" -enable-kvm"
-OPTS+=" -machine type=q35,kernel_irqchip=on" # 'kernel_irqchip=on' for qemu >=4.0
-OPTS+=" -rtc base=localtime" # Windows uses localtime
+OPTS+=(-enable-kvm)
+OPTS+=(-machine type=q35,kernel_irqchip=on) # 'kernel_irqchip=on' for qemu >=4.0
+OPTS+=(-rtc base=localtime) # Windows uses localtime
 
 # CPU
-OPTS+=" -cpu host,migratable=no,+invtsc,hv_time,hv_relaxed,hv_vapic,hv_spinlocks=0x1fff,topoext"
-OPTS+=" -smp 16,sockets=1,cores=8,threads=2"
+OPTS+=(-cpu host,migratable=off,+invtsc,hv_time,hv_relaxed,hv_vapic,hv_spinlocks=0x1fff,topoext)
+OPTS+=(-smp 16,sockets=1,cores=8,threads=2)
 # `bcdedit.exe /set useplatformclock true` enables HPET timer in Windows and introduces stuttering (most noticable with mouse input over barrier)
 
+# SMBIOS / DMI (decrease VM detection chance)
+OPTS+=(-smbios type=0,vendor="$(smbios bios-vendor)",date="$(smbios bios-release-date)",version="$(smbios bios-version)") # 0=BIOS
+OPTS+=(-smbios type=1,manufacturer="$(smbios system-manufacturer)",product="$(smbios system-product-name)",version="$(smbios system-version)",uuid="$(smbios system-uuid)",family="$(smbios system-family)") # 1=System
+OPTS+=(-smbios type=2,manufacturer="$(smbios baseboard-manufacturer)",product="$(smbios baseboard-product-name)",version="$(smbios baseboard-version)") # 2=Baseboard
+
 # RAM
-OPTS+=" -m ${MEMORY}G"
+OPTS+=(-m ${MEMORY}G)
 if [ "$ENABLE_HUGEPAGES" = true ]; then
     # Clear cached memory to allow reserving
     sync # Save cached data to disk
@@ -145,42 +156,42 @@ if [ "$ENABLE_HUGEPAGES" = true ]; then
     hugepages=$(( ${MEMORY} * 1024 / 2 ))
     echo "Huge Pages: ${hugepages}"
     echo $hugepages > /proc/sys/vm/nr_hugepages
-    OPTS+=" -mem-path /dev/hugepages"
-    OPTS+=" -mem-prealloc"
+    OPTS+=(-mem-path /dev/hugepages)
+    OPTS+=(-mem-prealloc)
 fi
 
 # UEFI/BIOS
-OPTS+=" -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.secboot.fd" # Win11 requires Secure Boot (available, not enabled)
-OPTS+=" -drive if=pflash,format=raw,file=/home/jonpas/images/vm/OVMF_VARS-win-ovmf.fd"
+OPTS+=(-drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.secboot.fd) # Win11 requires Secure Boot (available, not enabled)
+OPTS+=(-drive if=pflash,format=raw,file=/home/jonpas/images/vm/OVMF_VARS-win-ovmf.fd)
 
 # Drives
-OPTS+=" -device virtio-scsi-pci,id=scsi0"
+OPTS+=(-device virtio-scsi-pci,id=scsi0)
 
-OPTS+=" -drive file=/dev/disk/by-id/ata-Samsung_SSD_860_EVO_500GB_S3Z2NB2KA62661K,format=raw,index=0,if=none,aio=native,cache=none,id=drive0" # 'scsi-block' requires '/by-id/'
-OPTS+=" -device scsi-hd,drive=drive0,bus=scsi0.0,bootindex=1,rotation_rate=1" # 'scsi-block' for stats and SMART inside guest, 'scsi-hd' for discard/trim
+OPTS+=(-drive file=/dev/disk/by-id/ata-Samsung_SSD_860_EVO_500GB_S3Z2NB2KA62661K,format=raw,index=0,if=none,aio=native,cache=none,id=drive0) # 'scsi-block' requires '/by-id/'
+OPTS+=(-device scsi-hd,drive=drive0,bus=scsi0.0,bootindex=1,rotation_rate=1) # 'scsi-block' for stats and SMART inside guest, 'scsi-hd' for discard/trim
 
-OPTS+=" -drive file=/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S4J4NM0R706278T,format=raw,index=1,if=none,aio=native,cache=none,id=drive1" # 'scsi-block' requires '/by-id/'
-OPTS+=" -device scsi-hd,drive=drive1,bus=scsi0.0,rotation_rate=1" # 'scsi-block' for stats and SMART inside guest, 'scsi-hd' for discard/trim
+OPTS+=(-drive file=/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_2TB_S4J4NM0R706278T,format=raw,index=1,if=none,aio=native,cache=none,id=drive1) # 'scsi-block' requires '/by-id/'
+OPTS+=(-device scsi-hd,drive=drive1,bus=scsi0.0,rotation_rate=1) # 'scsi-block' for stats and SMART inside guest, 'scsi-hd' for discard/trim
 
 # Create image: qemu-img create -f raw name.img 10G -o preallocation=full (-o cluster_size=16K for qcow2)
 # Convert qcow2 to raw: qemu-img convert -p -O raw source.qcow2 target.img -o preallocation=full
 # Resize image: qemu-img resize -f raw --preallocation=full source.img +5G
-#OPTS+=" -device virtio-scsi-pci,id=scsi1"
-#OPTS+=" -drive file=/home/jonpas/Data/images/vm/data.img,format=raw,index=2,media=disk,if=none,aio=native,cache=none,id=drive2"
-#OPTS+=" -device scsi-hd,drive=drive2,bus=scsi1.0,rotation_rate=7200"
+#OPTS+=(-device virtio-scsi-pci,id=scsi1)
+#OPTS+=(-drive file=/home/jonpas/Data/images/vm/data.img,format=raw,index=2,media=disk,if=none,aio=native,cache=none,id=drive2)
+#OPTS+=(-device scsi-hd,drive=drive2,bus=scsi1.0,rotation_rate=7200)
 
-OPTS+=" -drive file=/home/jonpas/images/windows.iso,index=3,media=cdrom"
-OPTS+=" -drive file=/home/jonpas/images/virtio-win.iso,index=4,media=cdrom"
+OPTS+=(-drive file=/home/jonpas/images/windows.iso,index=3,media=cdrom)
+OPTS+=(-drive file=/home/jonpas/images/virtio-win.iso,index=4,media=cdrom)
 
 # Network
-OPTS+=" -net none"
-OPTS+=" -net nic,model=virtio" # 'virtio' may cause connection drop after a while without 'fix_virtio' patch (in qemu >=4.0)
-OPTS+=" -net bridge,br=virbr0" # -net user #,smb=/home/jonpas/Storage/"
+OPTS+=(-net none)
+OPTS+=(-net nic,model=virtio) # 'virtio' may cause connection drop after a while without 'fix_virtio' patch (in qemu >=4.0)
+OPTS+=(-net bridge,br=virbr0) # -net user #,smb=/home/jonpas/Storage/"
 
 # TPM (Win11 requires TPM 2.0)
-OPTS+=" -chardev socket,id=chrtpm,path=/tmp/qemu-tpm0/swtpm.sock"
-OPTS+=" -tpmdev emulator,id=tpm0,chardev=chrtpm"
-OPTS+=" -device tpm-tis,tpmdev=tpm0"
+OPTS+=(-chardev socket,id=chrtpm,path=/tmp/qemu-tpm0/swtpm.sock)
+OPTS+=(-tpmdev emulator,id=tpm0,chardev=chrtpm)
+OPTS+=(-device tpm-tis,tpmdev=tpm0)
 
 if [ ! -S /tmp/qemu-tpm0/swtpm.sock ]; then
     mkdir -p /tmp/qemu-tpm0
@@ -191,24 +202,24 @@ fi
 
 # GPU
 if [ "$ENABLE_PASSTHROUGH_GPU" = true ]; then
-    OPTS+=" -device pcie-root-port,chassis=0,bus=pcie.0,slot=0,id=root1"
+    OPTS+=(-device pcie-root-port,chassis=0,bus=pcie.0,slot=0,id=root1)
 
     rebind $PCI_GPU_VIDEO vfio-pci
-    OPTS+=" -device vfio-pci,host=$(echo $PCI_GPU_VIDEO | cut -c 6-),bus=root1,addr=00.0,multifunction=on"
+    OPTS+=(-device vfio-pci,host=$(echo $PCI_GPU_VIDEO | cut -c 6-),bus=root1,addr=00.0,multifunction=on)
     rebind $PCI_GPU_AUDIO vfio-pci
-    OPTS+=" -device vfio-pci,host=$(echo $PCI_GPU_AUDIO | cut -c 6-),bus=root1,addr=00.1"
+    OPTS+=(-device vfio-pci,host=$(echo $PCI_GPU_AUDIO | cut -c 6-),bus=root1,addr=00.1)
 fi
 
 if [ "$ENABLE_QEMU_GPU" = true ]; then
-    OPTS+=" -vga std"
+    OPTS+=(-vga std)
 else
-    OPTS+=" -vga none -nographic" # Disable QEMU VGA and graphics
+    OPTS+=(-vga none -nographic) # Disable QEMU VGA and graphics
 fi
 
 if [ "$ENABLE_LOOKINGGLASS" = true ]; then
     if [ "$LG_KVMFR_DEVICE" = true ]; then
-        OPTS+=" -device ivshmem-plain,id=shmem0,memdev=looking-glass"
-        OPTS+=" -object memory-backend-file,id=looking-glass,mem-path=/dev/kvmfr0,size=64M,share=yes"
+        OPTS+=(-device ivshmem-plain,id=shmem0,memdev=looking-glass)
+        OPTS+=(-object memory-backend-file,id=looking-glass,mem-path=/dev/kvmfr0,size=64M,share=on)
 
         # Create KVMFR device
         modprobe kvmfr static_size_mb=64
@@ -218,8 +229,8 @@ if [ "$ENABLE_LOOKINGGLASS" = true ]; then
         chown jonpas:jonpas /dev/kvmfr0 &&
         echo "KVMFR device owner changed" &
     else
-        OPTS+=" -device ivshmem-plain,memdev=ivshmem,bus=pcie.0"
-        OPTS+=" -object memory-backend-file,id=ivshmem,share=on,mem-path=/dev/shm/looking-glass,size=64M"
+        OPTS+=(-device ivshmem-plain,memdev=ivshmem,bus=pcie.0)
+        OPTS+=(-object memory-backend-file,id=ivshmem,share=on,mem-path=/dev/shm/looking-glass,size=64M)
 
         # Create shared memory
         if [ -f /dev/shm/looking-glass ]; then
@@ -233,7 +244,7 @@ if [ "$ENABLE_LOOKINGGLASS" = true ]; then
 
     # Spice connection
     if [ "$LG_SPICE_UNIX_SOCKET" = true ]; then
-        OPTS+=" -spice unix=on,addr=/run/user/1000/spice.sock,disable-ticketing=on" # Unix socket
+        OPTS+=(-spice unix=on,addr=/run/user/1000/spice.sock,disable-ticketing=on) # Unix socket
 
         # Set owner of Unix socket file (remove if exists, wait to be created by QEMU, change owner)
         if [ -S /run/user/1000/spice.sock ]; then
@@ -243,78 +254,79 @@ if [ "$ENABLE_LOOKINGGLASS" = true ]; then
         chown jonpas:jonpas /run/user/1000/spice.sock &&
         echo "Spice socket owner changed" &
     else
-        OPTS+=" -spice port=5900,addr=127.0.0.1,disable-ticketing=on" # TCP
+        OPTS+=(-spice port=5900,addr=127.0.0.1,disable-ticketing=on) # TCP
     fi
 
     # Spice Agent (clipboard)
-    OPTS+=" -device virtio-serial-pci"
-    OPTS+=" -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0"
-    OPTS+=" -chardev spicevmc,id=spicechannel0,name=vdagent"
+    OPTS+=(-device virtio-serial-pci)
+    OPTS+=(-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0)
+    OPTS+=(-chardev spicevmc,id=spicechannel0,name=vdagent)
 fi
 
 # Mouse & Keyboard (pass one always in case of lock-ups or network issues)
-OPTS+=" -device virtio-mouse-pci"
-OPTS+=" -device virtio-keyboard-pci"
+OPTS+=(-device virtio-mouse-pci)
+OPTS+=(-device virtio-keyboard-pci)
 if [ "$ENABLE_PASSTHROUGH_MOUSEKEYBOARD" = true ]; then
-    OPTS+=" -usb -device usb-host,vendorid=0x046d,productid=0xc332" # Logitech G502 Mouse
-    OPTS+=" -usb -device usb-host,vendorid=0x046d,productid=0xc24d" # Logitech G710 Keyboard
+    OPTS+=(-usb -device usb-host,vendorid=0x046d,productid=0xc332) # Logitech G502 Mouse
+    OPTS+=(-usb -device usb-host,vendorid=0x046d,productid=0xc24d) # Logitech G710 Keyboard
 else
     # Secondary mouse (connected to USB controller - internal)
     if [ "$ENABLE_PASSTHROUGH_USB_CONTROLLER" = false ]; then
-        OPTS+=" -usb -device usb-host,vendorid=0x0458,productid=0x0154" # KYE Systems Bluetooth Mouse
+        OPTS+=(-usb -device usb-host,vendorid=0x0458,productid=0x0154) # KYE Systems Bluetooth Mouse
     fi
 
     # evdev (lctrl + rctrl to swap, no macro keys)
-    OPTS+=" -object input-linux,id=kbd,evdev=/dev/input/by-path/pci-0000:0d:00.3-usb-0:4:1.0-event-kbd,grab_all=on,repeat=on" # Logitech G710 Keyboard
+    OPTS+=(-object input-linux,id=kbd,evdev=/dev/input/by-path/pci-0000:0d:00.3-usb-0:4:1.0-event-kbd,grab_all=on,repeat=on) # Logitech G710 Keyboard
 
     if [ "$ENABLE_EVDEV_MOUSE" = true ]; then
-        OPTS+=" -object input-linux,id=mouse,evdev=/dev/input/by-path/pci-0000:07:00.1-usb-0:3:1.0-event-mouse" # Logitech G502 Mouse
+        OPTS+=(-object input-linux,id=mouse,evdev=/dev/input/by-path/pci-0000:07:00.1-usb-0:3:1.0-event-mouse) # Logitech G502 Mouse
     fi
 fi
 
 # USB Controller (extension card)
 if [ "$ENABLE_PASSTHROUGH_USB_CONTROLLER" = true ]; then
     rebind 0000:05:00.0 vfio-pci # PCIe USB Card
-    OPTS+=" -device vfio-pci,host=05:00.0,bus=root1,addr=00.4"
+    OPTS+=(-device vfio-pci,host=05:00.0,bus=root1,addr=00.4)
 fi
 
 # USB devices (connected to USB controller)
 if [ "$ENABLE_PASSTHROUGH_USB_CONTROLLER" = false ]; then
-    OPTS+=" -usb -device usb-host,vendorid=0x131d,productid=0x0158" # Natural Point TrackIR 5 Pro Head Tracker
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb10a" # ThrustMaster T.16000M Joystick
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb687" # ThrustMaster TWCS Throttle
-    OPTS+=" -usb -device usb-host,vendorid=0x06a3,productid=0x0763" # Saitek (Logitech) G Flight Rudder Pedal
+    OPTS+=(-usb -device usb-host,vendorid=0x131d,productid=0x0158) # Natural Point TrackIR 5 Pro Head Tracker
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb10a) # ThrustMaster T.16000M Joystick
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb687) # ThrustMaster TWCS Throttle
+    OPTS+=(-usb -device usb-host,vendorid=0x06a3,productid=0x0763) # Saitek (Logitech) G Flight Rudder Pedal
 fi
 
 # USB devices (not connected to USB controller)
-OPTS+=" -usb -device usb-host,vendorid=0x0810,productid=0x0003" # Trust USB Gamepad
+OPTS+=(-usb -device usb-host,vendorid=0x0810,productid=0x0003) # Trust USB Gamepad
 
 if [ "$ENABLE_PASSTHROUGH_WHEEL" = true ]; then
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb677" # Thrustmaster T150 FFB Wheel (ID 1 - Linux reads it as either ID)
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb65d" # Thrustmaster T150 FFB Wheel (ID 2 - Linux reads it as either ID)
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb676" # Thrustmaster T150 FFB Wheel (Bootloader - Firmware update)
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb660" # Thrustmaster TH8A Shifter (T500 RS Gear shift)
-    OPTS+=" -usb -device usb-host,vendorid=0x044f,productid=0xb672" # Thrustmaster TH8A Shifter (Bootloader - Firmeware update)
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb677) # Thrustmaster T150 FFB Wheel (ID 1 - Linux reads it as either ID)
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb65d) # Thrustmaster T150 FFB Wheel (ID 2 - Linux reads it as either ID)
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb676) # Thrustmaster T150 FFB Wheel (Bootloader - Firmware update)
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb660) # Thrustmaster TH8A Shifter (T500 RS Gear shift)
+    OPTS+=(-usb -device usb-host,vendorid=0x044f,productid=0xb672) # Thrustmaster TH8A Shifter (Bootloader - Firmeware update)
 fi
 
 # Sound
 if [ "$ENABLE_PASSTHROUGH_AUDIO" = true ]; then
     rebind 0000:0d:00.4 vfio-pci # Audio
-    OPTS+=" -device vfio-pci,host=00:0d.0,bus=root1,addr=00.3"
+    OPTS+=(-device vfio-pci,host=00:0d.0,bus=root1,addr=00.3)
 else
     # JACK (PipeWire)
     export PIPEWIRE_RUNTIME_DIR=/run/user/1000
     export PIPEWIRE_LATENCY=512/48000
-    OPTS+=" -audiodev jack,id=hda0,in.client-name=win-vm,out.client-name=win-vm,out.connect-ports=Starship.*:playback_F[LR],in.connect-ports=Starship.*:capture_F[LR]"
+    OPTS+=(-audiodev jack,id=hda0,in.client-name=win-vm,out.client-name=win-vm,out.connect-ports=Starship.*:playback_F[LR],in.connect-ports=Starship.*:capture_F[LR])
 
-    OPTS+=" -device ich9-intel-hda"
-    OPTS+=" -device hda-micro,audiodev=hda0"
+    OPTS+=(-device ich9-intel-hda)
+    OPTS+=(-device hda-micro,audiodev=hda0)
 fi
 
 
 # VM START
 pkill -RTMIN+3 i3blocks
-qemu-system-x86_64 $OPTS
+echo "OPTS:${OPTS[@]}"
+qemu-system-x86_64 "${OPTS[@]}"
 
 
 # VM DEINIT
