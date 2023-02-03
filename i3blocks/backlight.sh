98e85e2730de087525ef97df0c4dac25c60e08fc
jonpas <jonpas33@gmail.com>
2023-02-03 15:43:11 +0100

    Fix brightness i3block

--- i3blocks/backlight.sh
+++ i3blocks/backlight.sh
@@ -6,8 +6,8 @@ fi
 
 sleep 0.1 # Wait for hardware to actually apply values
 
-actual_brightness=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
-max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+actual_brightness=$(cat /sys/class/backlight/acpi_video0/actual_brightness)
+max_brightness=$(cat /sys/class/backlight/acpi_video0/max_brightness)
 
 brightness=$(($actual_brightness * 100 / $max_brightness))
 
