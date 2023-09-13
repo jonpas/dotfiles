fd4381a7ad5cc9875438dbdecc79288912143e06
jonpas <jonpas33@gmail.com>
2023-09-13 20:48:25 +0200

    Improve UPS messages

--- powerpanel/pwrstatd-lowbatt.sh
+++ powerpanel/pwrstatd-lowbatt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Warning: The UPS's battery power is not enough, system will be shutdown soon!" | wall
+echo "Warning: UPS battery power is low. Shutting down!" | wall
 
-sudo -u jonpas DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "Warning: Utility battery is low. Shutting down!"
+sudo -u jonpas DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "Warning: UPS battery is low. Shutting down!"
--- powerpanel/pwrstatd-powerfail.sh
+++ powerpanel/pwrstatd-powerfail.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Warning: Utility power failure has occurred for a while, system will be shutdown soon!" | wall
+echo "Warning: Utility power has failed. Now running on battery." | wall
 
 sudo -u jonpas DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "Warning: Utility power has failed. Now running on battery."
