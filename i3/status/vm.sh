583e2261786a3d23a33f1defd69d89b9bbc2794b
jonpas <jonpas33@gmail.com>
2024-08-23 17:29:13 +0200

    Add mdev support to i3status-rs VM block

--- i3/status/vm.sh
+++ i3/status/vm.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 vm=" "
-gpu=""
+gpu=" "
 state="Idle"
 
 # Win VM
 if pgrep "win-(pci|gvt)" > /dev/null; then
     vm=" "
-    state="Idle"
 fi
 
 if [ $(hostname) = "loki" ]; then
@@ -15,10 +14,8 @@ if [ $(hostname) = "loki" ]; then
     gpu_drv=$(lspci -s 0c:00.0 -k | awk -F': ' '/Kernel driver in use/{print tolower($2)}')
     aud_drv=$(lspci -s 0c:00.1 -k | awk -F': ' '/Kernel driver in use/{print tolower($2)}')
 
-    gpu+="  "
     if [ "$gpu_drv" == "vfio-pci" ] && [ "$aud_drv" == "vfio-pci" ]; then
         gpu+="VFIO"
-        state="Idle"
     elif [[ ("$gpu_drv" == "nvidia" || "$gpu_drv" == "nouveau" || "$gpu_drv" == "amdgpu") && "$aud_drv" == "snd_hda_intel" ]]; then
         gpu+="HOST"
     else
@@ -33,6 +30,10 @@ if [ $(hostname) = "loki" ]; then
     else
         xset -led named "Scroll Lock"
     fi
+elif [ $(hostname) = "odin" ]; then
+    # GVT-g mediated device
+    mdevs=$(ls /sys/bus/mdev/devices/ | wc -l)
+    gpu+="$mdevs"
 fi
 
-echo {\"icon\": \"windows\", \"state\": \"$state\", \"text\": \"$vm$gpu\"}
+echo {\"icon\": \"windows\", \"state\": \"$state\", \"text\": \"$vm\ $gpu\"}
