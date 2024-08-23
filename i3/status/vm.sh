ccb6ed47c0deb684d149a11282c9680f49b19322
jonpas <jonpas33@gmail.com>
2024-08-23 03:49:31 +0200

    Improve i3status-rs battery indicators for odin and add support for WinGVT

--- i3/status/vm.sh
+++ i3/status/vm.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-#sleep 1 # Wait for signalling process a bit
-
 vm=" "
-gpu=" "
+gpu=""
 state="Idle"
 
 # Win VM
-if pgrep "win-pci" > /dev/null; then
+if pgrep "win-(pci|gvt)" > /dev/null; then
     vm=" "
     state="Idle"
 fi
 
-# GPU Pass-Through
-gpu_drv=$(lspci -s 0c:00.0 -k | awk -F': ' '/Kernel driver in use/{print tolower($2)}')
-aud_drv=$(lspci -s 0c:00.1 -k | awk -F': ' '/Kernel driver in use/{print tolower($2)}')
+if [ $(hostname) = "loki" ]; then
+    # GPU Pass-Through
+    gpu_drv=$(lspci -s 0c:00.0 -k | awk -F': ' '/Kernel driver in use/{print tolower($2)}')
+    aud_drv=$(lspci -s 0c:00.1 -k | awk -F': ' '/Kernel driver in use/{print tolower($2)}')
 
-if [ "$gpu_drv" == "vfio-pci" ] && [ "$aud_drv" == "vfio-pci" ]; then
-    gpu+="VFIO"
-    state="Idle"
-elif [[ ("$gpu_drv" == "nvidia" || "$gpu_drv" == "nouveau" || "$gpu_drv" == "amdgpu") && "$aud_drv" == "snd_hda_intel" ]]; then
-    gpu+="HOST"
-else
-    gpu+="NONE"
-    state="Warning"
+    gpu+="  "
+    if [ "$gpu_drv" == "vfio-pci" ] && [ "$aud_drv" == "vfio-pci" ]; then
+        gpu+="VFIO"
+        state="Idle"
+    elif [[ ("$gpu_drv" == "nvidia" || "$gpu_drv" == "nouveau" || "$gpu_drv" == "amdgpu") && "$aud_drv" == "snd_hda_intel" ]]; then
+        gpu+="HOST"
+    else
+        gpu+="NONE"
+        state="Warning"
+    fi
+
+    # Barrier (Scroll Lock LED as locked to screen indicator)
+    last_state_log=$(cat ~/.barriers.log | grep -a "locked" | tail -n 1)
+    if grep --quiet " locked" <<< $last_state_log; then
+        xset led named "Scroll Lock"
+    else
+        xset -led named "Scroll Lock"
+    fi
 fi
 
-# Barrier (Scroll Lock LED as locked to screen indicator)
-last_state_log=$(cat ~/.barriers.log | grep -a "locked" | tail -n 1)
-if grep --quiet " locked" <<< $last_state_log; then
-    xset led named "Scroll Lock"
-else
-    xset -led named "Scroll Lock"
-fi
-
-echo {\"icon\": \"windows\", \"state\": \"$state\", \"text\": \" $vm\ $gpu\"}
+echo {\"icon\": \"windows\", \"state\": \"$state\", \"text\": \"$vm$gpu\"}
