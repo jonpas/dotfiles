c0427f2a77c4de3a0b07a99aceecb2ebf89c73f9
jonpas <jonpas33@gmail.com>
2024-08-25 01:59:58 +0200

    Power off Bluetooth on odin unlock (support return from suspend)

--- i3/lock.sh
+++ i3/lock.sh
@@ -19,7 +19,18 @@ if [ "$dark" = "true" ]; then
     ~/dotfiles/rgb/rgb.sh off &
 fi
 
+if [ $(hostname) = "odin" ]; then
+    bt=$(rofi-bluetooth --status)
+    if [[ "${bt:0:2}" == "" ]]; then
+        bluetooth_off=true
+    fi
+fi
+
 i3lock -i /tmp/screenshotblur.png --nofork # no fork waits for unlock to continue executing
+
+if [ ! -z $bluetooth_off ]; then
+    bluetoothctl power off
+fi
 
 # Unlock
 if [ "$dark" = "true" ]; then
